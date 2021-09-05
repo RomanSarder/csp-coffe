f30c2d076f06a80316b006dd7108cbd547edae79
RomanSarder <roman.sarder@yandex.ru>
2021-09-05 19:16:26 +0300

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleDirectories: ["node_modules", "src"],
+  testMatch: ["<rootDir>/test/**/*.test.[jt]s?(x)"],
+  transform: {
+    "^.+\\.[jt]s(x)?$": "ts-jest",
+  },
+  collectCoverageFrom: [
+    "src/**/*.[jt]s?(x)",
+    "!coverage/**",
+    "!node_modules/**",
+  ],
+  coverageReporters: ["text", "lcov", "json", "text", "clover", "cobertura"],
+  coveragePathIgnorePatterns: ["tests/", "node_modules/"],
+  moduleNameMapper: {
+    "^@Lib/(.*)$": "<rootDir>/src/$1",
+  },
+  coverageDirectory: "<rootDir>/coverage/",
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "test/tsconfig.json",
+    },
+    __DEV__: true,
+  },
+};
---
+++ test/add.test.ts
@@ -0,0 +1,5 @@
+import { add } from '@Lib/index';
+
+it('should add two numbers', () => {
+    expect(add(2, 2)).toEqual(4);
+})
