df10691563bbba26eed149a38b6d9f9ff74f9d68
RomanSarder <roman.sarder@yandex.ru>
2021-10-20 00:08:56 +0300

    write test cases for non immediately available operations

--- test/operators/alts.test.ts
+++ test/operators/alts.test.ts
@@ -1,6 +1,8 @@
+import { BufferType } from '@Lib/buffer';
 import { makeChannel } from '@Lib/channel';
+import { eventLoopQueue } from '@Lib/internal';
 import { alts } from '@Lib/operators';
-import { makePut } from '@Lib/operators/internal';
+import { makePut, releasePut } from '@Lib/operators/internal';
 
 describe('alts', () => {
     describe('when given only channels', () => {
@@ -17,7 +19,7 @@ describe('alts', () => {
         });
     });
     describe('when given only put definitions', () => {
-        describe('when any channel has space in buffer', () => {
+        describe('when any channel has space in buffer immediately', () => {
             it('should put value into the channel with buffer space', async () => {
                 const ch1 = makeChannel<string>();
                 const ch2 = makeChannel<number>();
@@ -25,8 +27,41 @@ describe('alts', () => {
 
                 const result = await alts([
                     [ch1, 'test1'],
-                    [ch2, '10'],
+                    [ch2, 10],
                 ]);
+                expect(result.value).toEqual(true);
+                expect(result.channel.is(ch1)).toEqual(true);
+            });
+        });
+
+        describe('when no channel as space in buffer immediately', () => {
+            it('should wait until any operation succeeds', async () => {
+                const ch1 = makeChannel<string>();
+                const ch2 = makeChannel<number>(BufferType.FIXED, 2);
+                const spy = jest.fn();
+                makePut(ch2, 10);
+                makePut(ch2, 30);
+                makePut(ch1, 'test1');
+
+                alts([
+                    [ch1, 'test2'],
+                    [ch2, 20],
+                ]).then((res) => {
+                    spy(res);
+                });
+
+                await eventLoopQueue();
+
+                expect(spy).not.toHaveBeenCalled();
+
+                await eventLoopQueue();
+
+                releasePut(ch2);
+                releasePut(ch2);
+
+                await eventLoopQueue();
+
+                expect(spy).toHaveBeenCalledWith({ value: true, channel: ch2 });
             });
         });
     });
