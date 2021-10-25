610c229e244ced2e4bc7e7bcc01d4e52adc23a33
RomanSarder <roman.sarder@yandex.ru>
2021-10-25 23:05:07 +0300

    write sync worker

---
+++ src/go/worker/syncWorker.ts
@@ -0,0 +1,60 @@
+import { Command } from '../entity';
+import { isInstruction } from '../utils';
+import { isGenerator } from './shared';
+
+// eslint-disable-next-line consistent-return
+export function* syncWorker<G extends Generator>(iterator: G): Generator {
+    let nextIteratorValue;
+    try {
+        nextIteratorValue = iterator.next();
+    } catch (e) {
+        return iterator.throw(e);
+    }
+
+    while (!nextIteratorValue.done) {
+        yield nextIteratorValue.value;
+        try {
+            if (isGenerator(nextIteratorValue.value)) {
+                let innerIterator;
+                try {
+                    innerIterator = syncWorker(nextIteratorValue.value);
+                    let innerNextIteratorValue = innerIterator.next();
+
+                    while (!innerNextIteratorValue.done) {
+                        yield innerNextIteratorValue.value;
+
+                        innerNextIteratorValue = innerIterator.next();
+                    }
+                } catch (e) {
+                    innerIterator?.throw(e);
+                }
+            } else if (
+                isInstruction(nextIteratorValue.value) &&
+                nextIteratorValue.value.command === Command.EXECUTE
+            ) {
+                let innerIterator;
+                try {
+                    innerIterator = syncWorker(
+                        nextIteratorValue.value.value as Generator,
+                    );
+                    nextIteratorValue = innerIterator.next();
+                    console.log('inner value', nextIteratorValue);
+
+                    while (!nextIteratorValue.done) {
+                        yield nextIteratorValue.value;
+
+                        nextIteratorValue = innerIterator.next();
+                    }
+                } catch (e) {
+                    console.log('inner err', innerIterator?.next());
+                    nextIteratorValue = innerIterator?.throw(e);
+                }
+            }
+            nextIteratorValue = iterator.next();
+        } catch (e) {
+            nextIteratorValue = iterator.throw(e);
+        }
+    }
+
+    return nextIteratorValue;
+}
