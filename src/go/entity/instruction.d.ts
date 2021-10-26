641bb7634b1ff63832413045d6bdcfa79d3571f7
RomanSarder <roman.sarder@yandex.ru>
2021-10-26 22:42:33 +0300

    Modify instruction to contain value in meta

--- src/go/entity/instruction.d.ts
+++ src/go/entity/instruction.d.ts
@@ -1,9 +1,10 @@
 import { Command } from './command';
 
-export interface Instruction<T = unknown> {
+export interface Instruction<T = unknown, A = unknown> {
     command: Command;
     meta?: {
         name: string;
+        args: A;
     };
     value?: T;
 }
