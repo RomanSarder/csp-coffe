17af4197ceafed0ad0d9d701a6a7d485bd0e6997
RomanSarder <roman.sarder@yandex.ru>
2021-10-26 22:44:07 +0300

    adjust map typings to make test compile

--- src/operators/transformation/map.ts
+++ src/operators/transformation/map.ts
@@ -8,7 +8,10 @@ import { close, put } from '..';
 import { DEFAULT_RESULT_CHANNEL_CONFIG } from '../shared/constants';
 import { iterate } from './iterate';
 
-export function map<Channels extends Channel<any>[], M = unknown>(
+export function map<
+    Channels extends Channel<any>[],
+    M extends NonNullable<any> = any,
+>(
     mapFn: (data: FlattenChannels<Channels>) => M,
     channels: Channels,
     {
@@ -19,7 +22,7 @@ export function map<Channels extends Channel<any>[], M = unknown>(
     const mappedCh = makeChannel<M>(bufferType, capacity);
     const promises = channels.map((ch) => {
         return iterate<FlattenChannels<Channels>>(async (data) => {
-            await put(mappedCh, mapFn(data));
+            await put<Channel<M>>(mappedCh, mapFn(data));
         }, ch);
     });
 
