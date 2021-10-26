86dd41fb5dc2a07549e2db11bb27ff9309bd88f3
RomanSarder <roman.sarder@yandex.ru>
2021-10-26 22:42:54 +0300

    modify execute instruction to work with operators and meta args

--- src/go/instructions/execute.ts
+++ src/go/instructions/execute.ts
@@ -2,14 +2,15 @@ import { Operator } from '@Lib/operators/operator.types';
 import { Command, Instruction } from '../entity';
 
 export function makeExecuteInstruction<
-    O extends Operator<any>,
-    G = O extends Operator<infer T> ? T : unknown,
->(operator: O): Instruction<G> {
+    O extends (...a: readonly any[]) => Operator<Generator>,
+>(makeOperator: O, ...args: Parameters<O>): Instruction<Generator> {
+    const operator = makeOperator(...args);
     return {
         command: Command.EXECUTE,
         value: operator.generator,
         meta: {
             name: operator.name,
+            args,
         },
     };
 }
