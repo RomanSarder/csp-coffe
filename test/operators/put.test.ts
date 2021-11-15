64a2120f5af535edcdcbcb161e691aae909349ad
RomanSarder <roman.sarder@yandex.ru>
2021-11-15 21:12:39 +0200

    fix put tests

--- test/operators/put.test.ts
+++ test/operators/put.test.ts
@@ -1,22 +1,22 @@
 import { CreatableBufferType } from '@Lib/buffer';
 import { makeChannel } from '@Lib/channel';
-import { asyncGeneratorProxy } from '@Lib/go/worker';
-import { putGenerator, close } from '@Lib/operators';
+import { asyncGeneratorProxy } from '@Lib/asyncGeneratorProxy';
+import { put, close } from '@Lib/operators';
 import { makePut, releasePut } from '@Lib/operators/internal';
 
 describe('put', () => {
     it('should put a value to channel', async () => {
         const ch = makeChannel();
-        const iterator = asyncGeneratorProxy(putGenerator(ch, 'test1'));
-        const result1 = await iterator.next();
-        const result2 = await iterator.next(result1);
-        await iterator.next(result2);
+        const iterator = asyncGeneratorProxy(put(ch, 'test1'));
+        await iterator.next();
+        await iterator.next();
+        await iterator.next();
         expect(ch.putBuffer.getElementsArray()).toEqual(['test1']);
     });
 
     it('should throw error if trying to put null', async () => {
         const ch = makeChannel();
-        const iterator = asyncGeneratorProxy(putGenerator(ch, null));
+        const iterator = asyncGeneratorProxy(put(ch, null));
         expect(iterator.next()).rejects.toEqual(
             new Error('null values are not allowed'),
         );
@@ -27,9 +27,11 @@ describe('put', () => {
         it('should not put anything and reset the channel', async () => {
             const ch = makeChannel();
             close(ch);
-            const iterator = asyncGeneratorProxy(putGenerator(ch, 'test1'));
-            const result1 = await iterator.next();
-            await iterator.next(result1);
+            const iterator = asyncGeneratorProxy(put(ch, 'test1'));
+            await iterator.next();
+            await iterator.next();
+            const result = await iterator.next();
+            expect(result.done).toEqual(true);
             expect(ch.putBuffer.getElementsArray()).toEqual([]);
         });
     });
@@ -37,12 +39,12 @@ describe('put', () => {
     describe('when the channel is closed after the item is put', () => {
         it('should release put', async () => {
             const ch = makeChannel();
-            const iterator = asyncGeneratorProxy(putGenerator(ch, 'test1'));
-            const result1 = await iterator.next();
-            const result2 = await iterator.next(result1);
-            const result3 = await iterator.next(result2);
+            const iterator = asyncGeneratorProxy(put(ch, 'test1'));
+            await iterator.next();
+            await iterator.next();
+            await iterator.next();
             close(ch);
-            await iterator.next(result3);
+            expect((await iterator.next()).done).toEqual(true);
             expect(ch.putBuffer.getElementsArray()).toEqual([]);
         });
     });
@@ -51,28 +53,23 @@ describe('put', () => {
         describe('when there is no pending take', () => {
             it('should not block put if no take request is there', async () => {
                 const ch = makeChannel(CreatableBufferType.DROPPING, 2);
-                const iterator = asyncGeneratorProxy(putGenerator(ch, 'test1'));
-                const result1 = await iterator.next();
-                const result2 = await iterator.next(result1);
-                expect((await iterator.next(result2)).done).toEqual(true);
+                const iterator = asyncGeneratorProxy(put(ch, 'test1'));
+                await iterator.next();
+                await iterator.next();
+                expect((await iterator.next()).done).toEqual(true);
             });
 
             describe('when the buffer is full', () => {
                 it('should block put request if buffer is full', async () => {
                     const ch = makeChannel(CreatableBufferType.FIXED, 2);
-                    const iterator = asyncGeneratorProxy(
-                        putGenerator(ch, 'test1'),
-                    );
+                    const iterator = asyncGeneratorProxy(put(ch, 'test1'));
                     makePut(ch, 'test11');
                     makePut(ch, 'test12');
-                    const result1 = await iterator.next();
-                    const result2 = await iterator.next(result1);
-                    const result3 = await iterator.next(result2);
-                    expect(result3.value).toEqual({
-                        command: 'PARK',
-                    });
+                    await iterator.next();
+                    expect((await iterator.next()).done).toEqual(false);
                     releasePut(ch);
-                    expect((await iterator.next(result3)).done).toEqual(true);
+                    await iterator.next();
+                    expect((await iterator.next()).done).toEqual(true);
                     expect(ch.putBuffer.getElementsArray()).toEqual([
                         'test12',
                         'test1',
