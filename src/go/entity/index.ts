8a1ad649a61638a7e224348a599e6b7fb5f2ba22
RomanSarder <roman.sarder@yandex.ru>
2021-11-22 22:28:14 +0200

    remove redundant files

--- src/go/entity/index.ts
+++ src/go/entity/index.ts
@@ -1,8 +1,6 @@
 export type { Instruction } from './instruction';
 export type { CallInstruction } from './callInstruction';
-export type { ScheduleInstruction } from './scheduleInstruction';
 export type { ForkInstruction } from './forkInstruction';
-export type { Task } from './task';
 
 export * from './event';
 export * from './instructionType';
--- src/go/entity/task.d.ts
+++
@@ -1,4 +0,0 @@
-export type Task = {
-    generator: Generator;
-    isFork: boolean;
-};
