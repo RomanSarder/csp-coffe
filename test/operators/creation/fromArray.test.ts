360deba21b3f45a07ac6345d4023c03e53c60551
RomanSarder <roman.sarder@yandex.ru>
2021-12-05 15:20:57 +0200

    fromArray tests fix

--- test/operators/creation/fromArray.test.ts
+++ test/operators/creation/fromArray.test.ts
@@ -1,20 +1,21 @@
 import { eventLoopQueue } from '@Lib/internal';
-import { fromArray, take } from '@Lib/operators';
+import { fromArray } from '@Lib/operators/creation/fromArray';
+import { takeAsync } from '@Lib/operators/takeAsync';
 
 describe('fromArray', () => {
     it('should create channel with contents of array in buffer', async () => {
         const array = ['test1', 'test2'];
         const ch = fromArray(array);
-        expect(await take(ch)).toEqual('test1');
-        expect(await take(ch)).toEqual('test2');
+        expect(await takeAsync(ch)).toEqual('test1');
+        expect(await takeAsync(ch)).toEqual('test2');
         await eventLoopQueue();
     });
 
     it('should close channel once all values are taken', async () => {
         const array = ['test1', 'test2'];
         const ch = fromArray(array);
-        expect(await take(ch)).toEqual('test1');
-        expect(await take(ch)).toEqual('test2');
+        expect(await takeAsync(ch)).toEqual('test1');
+        expect(await takeAsync(ch)).toEqual('test2');
         await eventLoopQueue();
         expect(ch.isClosed).toEqual(true);
     });
