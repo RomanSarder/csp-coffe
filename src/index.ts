f7fe2c0254cb6b32627cf37eac91ed37e269e05a
RomanSarder <roman.sarder@yandex.ru>
2022-02-14 17:10:22 +0200

    disable channel id overwrite

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * as CancellablePromise from './cancellablePromise';
-export * as Buffer from './buffer';
-export * as Channel from './channel';
-export * as Operators from './operators';
-export * as Go from './go';
-export * as Test from './testGeneratorRunner';
